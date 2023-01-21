706c0fa1868877e1800cc450ae8e7794c244d01e
JasurbekYoqubjonov <jasurbekdeveloper5678@gmail.com>
2023-01-21 10:35:13 +0500

    h

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Important from "./component/Important"
+import Shop from "./component/Shop"
+import Home from "./component/Home"
+import Navbar from "./component/Navbar"
+import Loyalty from './component/Loyalty';
+import About from './component/About';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/important' element={<Important/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/loyalty' element={<Loyalty/>}/>
+        <Route path='/about' element={<About/>}/>
+       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
